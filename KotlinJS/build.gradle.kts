24f758159fcd33cdf90d3055c9c0b41e1357fe87
zsh <2905154639@qq.com>
2024-12-22 16:28:40 +0800

    use npm, use axios, use js-yaml

--- KotlinJS/build.gradle.kts
+++ KotlinJS/build.gradle.kts
@@ -8,8 +8,10 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
 dependencies {
+    // 只能使用 npm 平台的库
+    commonMainImplementation(npm("js-yaml", "4.1.0"))
+    commonMainImplementation(npm("axios", "1.7.9"))
 }
 kotlin {
     js {
--- KotlinJS/src/jsMain/kotlin/Main.kt
+++ KotlinJS/src/jsMain/kotlin/Main.kt
@@ -1,10 +1,48 @@
 package edu.tyut
 
 import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.Element
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+fun parseYaml(yaml: String): dynamic {
+    val jsYaml = js("require('js-yaml')")
+    return jsYaml.load(yaml)
+}
+
 fun main() {
-    console.log("Hello from Kotlin JS")
-    document.write("Hello Kotlin JS!")
+    val yamlData = """
+        name: Kotlin
+        version: 1.9.0
+        dataList:
+            - 1
+            - 2
+    """
+    val result: String = parseYaml(yamlData)
+    document.writeln("Parsed YAML: ${JSON.stringify(result)}")
+
+    window.onload = {
+        val click: Element? = document.getElementById("click")
+        // document.writeln("click: $click")
+        val axios: dynamic = js("require('axios')")
+        click?.addEventListener("click", {
+            try {
+                // 有跨域问题
+                val response: dynamic = axios.get("http://localhost:8080/Spring_MVC_01/hello")
+                // document.writeln("Response: ${response.data}")
+                console.log("Response: ${
+                    response.then { response: dynamic ->
+                        println("Response: ${response.data}")
+                    }
+                    .catch { error: dynamic ->
+                        println("Error: ${error.message}")
+                    }
+                }")
+            } catch (e: dynamic) {
+                console.log("Error: ${e.message}")
+            }
+        })
+    }
+
+
 }
